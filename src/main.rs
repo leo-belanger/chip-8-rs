66831f884b5ea06d62d91b3976ba8d24751b99c6
Leo Belanger <belanger.leo01@gmail.com>
2022-11-14 09:56:01 -0500

    Better error handling on cpu creation and WIP for args parsing with Clap.
    I chose clap quickly because it was the most popular (I think), gonna check if this is the right choice before moving on.

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,34 @@ mod keypad;
 mod ram;
 mod speaker;
 
+use clap::{self, Parser};
 use cpu::CPU;
 use std::{env, process};
+
+#[derive(clap::Parser, Debug)]
+struct Args {
+    #[arg(default_value_t = String::from("programs/test_opcode.ch8"))]
+    program_path: String,
+
+    #[arg(default_value_t = 50)]
+    max_instructions_per_frames: usize,
+}
 
 fn main() {
     println!("Chip 8 emulator");
 
+    let args = Args::parse();
+
     let args: Vec<String> = env::args().collect();
     let file_path = &args[1];
 
-    let mut cpu = CPU::new().unwrap();
+    let mut cpu = match CPU::new() {
+        Ok(new_cpu) => new_cpu,
+        Err(e) => {
+            eprintln!("Error while trying to create the CPU: {e}");
+            process::exit(1)
+        }
+    };
 
     if let Err(e) = cpu.run(file_path) {
         eprintln!("Chip-8 error: {e}");
